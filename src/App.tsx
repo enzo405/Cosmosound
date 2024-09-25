26892309dab998fb4fee88550180fb67ad3e61ca
enzo-chaboisseau <enzo.chaboisseau@gmail.com>
2024-09-25 11:04:05 +0200

    re-init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from "./components/template/Footer";
+import Header from './components/template/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
