51020da26d24a124945985f5e6a82ac60097164a
enzo-chaboisseau <enzo.chaboisseau@gmail.com>
2024-11-05 01:07:46 +0100

    Add finished animation

--- src/components/Template/Sidebar/SidebarItem.tsx
+++ src/components/Template/Sidebar/SidebarItem.tsx
@@ -1,7 +1,7 @@
 import { Icon } from "components/Icon";
 import { IconName } from "constants/iconName";
-import { ReactElement } from "react";
-import { useNavigate } from "react-router-dom";
+import { ReactElement, useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 interface SidebarItemProps {
   iconName: IconName;
@@ -17,29 +17,38 @@ export default function SidebarItem({
   path,
 }: SidebarItemProps): ReactElement {
   const navigate = useNavigate();
+  const location = useLocation();
+  const isActive = location.pathname === path;
 
-  const isActive = (path: string) => {
-    return window.location.pathname === path;
-  };
-
-  let active = isActive(path);
   let classes = "";
-  if (active) {
-    classes += "bg-sidebar-item-bg text-primary-orange";
+  if (isActive) {
+    classes += "text-primary-orange";
   } else {
-    classes += "hover:bg-sidebar-item-bg-hover text-dark-custom";
+    classes += "text-dark-custom";
   }
+
   return (
     <div
       onClick={() => navigate(path)}
-      className={`${classes} w-auto sm:w-11/12 flex flex-col px-2 xsm:px-4 py-1 cursor-pointer rounded-t-2xl sm:rounded-e-full justify-center sm:justify-normal sm:flex-row`}>
-      <span className="flex justify-center sm:justify-normal sm:w-auto">
+      className={`${classes} relative w-auto sm:w-11/12 h-full sm:h-auto flex gap-2 items-center px-2 xsm:px-4 py-1 cursor-pointer rounded-t-2xl sm:rounded-e-full sm:flex-row overflow-hidden`}>
+      {/* Desktop Animated Background */}
+      <span
+        className={`hidden sm:block absolute inset-0 h-full bg-soft-beige transition-all duration-200 ${isActive ? "w-full" : "w-0"}`}
+      />
+
+      {/* Mobile Animated Background */}
+      <span
+        className={`block sm:hidden absolute bottom-0 inset-x-0 w-full bg-soft-beige transition-all duration-200 ${isActive ? "h-full" : "h-0"}`}
+      />
+
+      {/* Icon and Text */}
+      <span className="flex justify-center sm:justify-normal sm:w-auto z-10">
         <Icon
-          iconName={active ? iconNameActive : iconName}
-          className="min-h-6 h-6 min-w-6 w-6 sm:h-8 sm:w-8 flex"
+          iconName={isActive ? iconNameActive : iconName}
+          className="min-h-6 h-6 min-w-6 w-6 sm:h-8 sm:w-8"
         />
       </span>
-      <a className="flex w-full justify-center items-center text-sm font-bold sm:text-xl sm:justify-normal sm:ml-2">
+      <a className="flex w-full justify-center items-center text-sm font-bold sm:text-xl sm:justify-normal sm:ml-2 z-10">
         {text}
       </a>
     </div>
