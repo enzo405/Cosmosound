ea41e5fd3e3cee822a3497ac9103dc59c91f7236
enzo-chaboisseau <enzo.chaboisseau@gmail.com>
2024-11-13 17:50:40 +0100

    Scrollable Box arrows in the corner

--- src/components/box/ScrollableBox.tsx
+++ src/components/box/ScrollableBox.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement, useEffect, useRef, useState } from "react";
 import ArrowLeft from "components/icons/ArrowLeft";
 import ArrowRight from "components/icons/ArrowRight";
-import { titleStyle } from "./styles";
 
 interface ScrollableBoxProps {
   title: string;
@@ -38,25 +37,27 @@ export default function ScrollableBox({ title, children }: ScrollableBoxProps): 
   }, []);
 
   return (
-    <div className="bg-box-bg rounded-3xl h-min w-full flex flex-col pl-4">
-      <div className="w-full flex py-4">
-        <span className={titleStyle}>{title}</span>
+    <div className="bg-box-bg rounded-3xl h-min w-full flex flex-col">
+      <div className="w-full flex py-4 pl-4">
+        <span className="mr-auto text-lg font-bs font-light">{title}</span>
+        <span className="relative flex flex-row gap-3 mr-4">
+          <span
+            onClick={scrollLeft}
+            className={`rounded-full flex justify-center items-center w-6 h-6 ${canScrollLeft ? "cursor-pointer hover:bg-gray-200" : ""} `}>
+            <ArrowLeft isActiv={canScrollLeft} />
+          </span>
+          <span
+            onClick={scrollRight}
+            className={`rounded-full flex justify-center items-center w-6 h-6 ${canScrollRight ? "cursor-pointer hover:bg-gray-200" : ""}`}>
+            <ArrowRight isActiv={canScrollRight} />
+          </span>
+        </span>
       </div>
       <div
         onScroll={updateScrollButtons}
         ref={scrollContainerRef}
-        className="relative flex flex-nowrap overflow-x-scroll w-full scroll-smooth items-center">
-        <span
-          onClick={scrollLeft}
-          className={`sticky left-0 flex items-center h-1/2 p-3 rounded-full ${canScrollLeft ? "cursor-pointer bg-gray-200" : ""}`}>
-          <ArrowLeft isActiv={canScrollLeft} />
-        </span>
+        className="flex flex-nowrap overflow-x-scroll w-full scroll-smooth pl-4">
         {children}
-        <span
-          onClick={scrollRight}
-          className={`sticky right-0 flex items-center h-1/2 p-3 rounded-full ${canScrollRight ? "cursor-pointer bg-gray-200" : ""}`}>
-          <ArrowRight isActiv={canScrollRight} />
-        </span>
       </div>
     </div>
   );
