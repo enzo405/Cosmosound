aaa7524f0e9b785e00cb1e7ea290b7b0e9cf4c74
enzo-chaboisseau <enzo.chaboisseau@gmail.com>
2024-12-09 14:56:56 +0100

    Added select-none to MusicItem

--- src/components/music/MusicItem.tsx
+++ src/components/music/MusicItem.tsx
@@ -102,7 +102,7 @@ export default function MusicItem({
         className={`group flex flex-row w-full p-1 justify-between h-16 ${showCatalogThumbnail ? "md:h-[72px] lg:h-20" : "md:h-[64px] lg:h-18"} ${playingMusic.id == music.id ? "bg-white-orange" : "hover:bg-secondary-orange"} rounded-xl`}>
         <div
           onDoubleClick={() => handleDoubleClickPlay(music)}
-          className="flex flex-row flex-grow justify-between xsm:pr-1 sm:pr-2 md:pr-4 lg:pr-36 cursor-pointer min-w-0">
+          className="flex flex-row flex-grow select-none justify-between xsm:pr-1 sm:pr-2 md:pr-4 lg:pr-36 cursor-pointer min-w-0">
           <div className="flex flex-row items-center w-full sm:max-w-80 sm:min-w-80 lg:min-w-[28rem] lg:max-w-[28rem] gap-1">
             <div
               onMouseEnter={() => setDisplayPlay(true)}
@@ -139,7 +139,7 @@ export default function MusicItem({
             </div>
             <div className="flex flex-col xsm:gap-1 h-full max-w-full overflow-hidden items-start justify-center">
               <span className="text-base text-dark-custom flex gap-1 xsm:gap-2 items-center w-full">
-                <span className="font-semibold select-none text-sm xsm:font-normal xsm:text-base truncate">
+                <span className="font-semibold text-sm xsm:font-normal xsm:text-base truncate">
                   {music.title}
                 </span>
                 <span
@@ -151,7 +151,7 @@ export default function MusicItem({
                   />
                 </span>
               </span>
-              <span className="w-full text-sm select-none text-dark-grey font-semibold flex flex-row gap-1">
+              <span className="w-full text-sm text-dark-grey font-semibold flex flex-row gap-1">
                 {showArtist && <ArtistInfo artist={artist} className="min-w-fit" />}
                 {showArtist && showCatalog && (
                   <span className="hidden lg:block flex-shrink-0">-</span>
--- src/components/music/MusicItemDelete.tsx
+++ src/components/music/MusicItemDelete.tsx
@@ -43,7 +43,7 @@ export default function MusicItemDelete({
     <div className="relative w-full" ref={musicItemRef}>
       <div
         className={`group flex flex-row w-full p-1 justify-between h-16 md:h-[64px] lg:h-18 ${playingMusic.id == music.id ? "bg-white-orange" : "hover:bg-dark-custom"} rounded-xl`}>
-        <div className="flex flex-row w-full flex-grow min-w-0 justify-between xsm:pr-1 sm:pr-2 md:pr-4 lg:pr-36 cursor-pointer">
+        <div className="flex flex-row w-full select-none flex-grow min-w-0 justify-between xsm:pr-1 sm:pr-2 md:pr-4 lg:pr-36 cursor-pointer">
           <div className="flex flex-row items-center w-full sm:max-w-80 sm:min-w-80 lg:min-w-[28rem] lg:max-w-[28rem] gap-1">
             <div className="relative p-0.5 h-5/6 xsm:h-full">
               <div
@@ -75,10 +75,10 @@ export default function MusicItemDelete({
               </span>
             </div>
           </div>
-          <div className="hidden xl:flex items-center select-none max-w-36 min-w-36 justify-center font-light text-sm">
+          <div className="hidden xl:flex items-center max-w-36 min-w-36 justify-center font-light text-sm">
             {formatTime(music.date_creation)}
           </div>
-          <div className="hidden lg:flex items-center select-none w-10 justify-center font-light text-sm">
+          <div className="hidden lg:flex items-center w-10 justify-center font-light text-sm">
             {formatDuration(music.duration)}
           </div>
         </div>
