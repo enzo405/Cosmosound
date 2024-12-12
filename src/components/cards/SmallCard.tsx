e932cd0cdda686d4beeddf68a154df99599cd3c4
enzo-chaboisseau <enzo.chaboisseau@gmail.com>
2024-12-12 13:37:35 +0100

    TROONKATE

--- src/components/cards/SmallCard.tsx
+++ src/components/cards/SmallCard.tsx
@@ -42,7 +42,7 @@ export default function SmallCard({
       onClick={(e) => handleOnClick(e)}
       className={`relative flex flex-col group cursor-pointer min-w-24 max-w-24 sm:min-w-32 sm:max-w-32 h-14 border border-dark-glassy rounded-[1.2rem] p-2 ${className}`}>
       <div className="h-full w-full flex items-center justify-center group-hover:underline underline-offset-2 text-sm font-medium truncate">
-        <span className={`${title.length > 10 ? "animate-scroll-text" : ""}`}>{title}</span>
+        <span className="truncate">{title}</span>
       </div>
       {displayLikeBtn && (
         <div id="likeBtn" className="z-10 absolute -bottom-2 -right-1">
