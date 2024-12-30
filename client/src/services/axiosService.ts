baed6677b13580e21153b81742a484de047c6b30
enzo-chaboisseau <enzo.chaboisseau@gmail.com>
2024-12-30 12:44:49 +0100

    Added receptor axios refreshToken

--- client/src/services/axiosService.ts
+++ client/src/services/axiosService.ts
@@ -6,4 +6,25 @@ const apiClient = axios.create({
   baseURL: "http://localhost:4000",
 });
 
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      if (originalRequest.url === "/auth/refresh") {
+        return Promise.reject(error);
+      }
+      try {
+        await apiClient.post("/auth/refresh");
+        originalRequest._retry = true;
+        return apiClient(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+  },
+);
+
 export { apiClient };
